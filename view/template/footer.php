cc92f542737f0ad7aefd03b2742fcf9cfb5d29ec
Adi Suryadin <adisuryadin598@gmail.com>
2024-05-15 13:35:48 +0800

    upload pertama

---
+++ view/template/footer.php
@@ -0,0 +1,17 @@
+          
+      </div>
+      <footer class="main-footer">
+        <div class="footer-left">
+          Copyright &copy; 2023 <div class="bullet"></div> Design By <a href="https://nauval.in/">Charlie Aprizal</a> | <a href='#' title='StokCoding.com' target='_blank'>210120049</a>
+          
+        </div>
+        <div class="footer-right">
+          Developer : <a href="#" target="blank">Charlie Aprizal</a> 
+          <!-- Create By <a href="#">Code Peka</a> -->
+        </div>
+      </footer>
+    </div>
+  </div>
+
+</body>
+</html>
